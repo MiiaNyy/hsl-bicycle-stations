698551ac607a7537995ee53baff7edc3e2294c28
MiiaNyy <mi.nyyssonen@gmail.com>
2022-07-29 15:34:08 +0300

    Connected mongodb as graphql datasource

---
+++ server/src/dataSources/journeys.js
@@ -0,0 +1,13 @@
+import { MongoDataSource } from "apollo-datasource-mongodb";
+
+class Journeys extends MongoDataSource {
+    async getJourneys () {
+        return this.model.find();
+    }
+    
+    async getJourney (id) {
+        return this.findById( id );
+    }
+}
+
+export default Journeys;
