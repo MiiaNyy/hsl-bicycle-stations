ef3055ccda174c66d5aa736b340adb8a2e03c9d7
MiiaNyy <mi.nyyssonen@gmail.com>
2022-08-02 12:35:25 +0300

    exctracted toCamelCase to owm module

---
+++ server/src/helpers/toCamelCase.js
@@ -0,0 +1,7 @@
+function toCamelCase (str) {
+	return str.replace( /(?:^.|[A-Z]|\b.)/g, function (letter, index) {
+		return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
+	} ).replace( /\s+/g, '' );
+}
+
+export default toCamelCase;
