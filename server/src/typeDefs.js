2bc560c46c382d516fd741837a5f22a8506c0ed0
MiiaNyy <mi.nyyssonen@gmail.com>
2022-08-08 14:32:55 +0300

    modified graphql schema to work with mongodb schema

--- server/src/typeDefs.js
+++ server/src/typeDefs.js
@@ -1,38 +1,36 @@
 import { gql } from 'apollo-server';
 
 const typeDefs = gql`
-    
+
     type Query {
         singleStation(id: ID!): Station!
         allStations: [Station]!
-        
-        getJourneys: [Journey]!
+		"Get all journeys depending on the amount"
+        getJourneys(amount:Int!): [Journey]!
         getJourney(id: ID!): Journey!
-        
-        insertJourneys: String
-
     }
 
     type Station {
-        id: ID!
+        id: Int!
         address: String!
         journeysStartingFrom: Int
         journeysEndingAt: Int
     }
 
+	
     type Journey {
         id: ID!
-        departureTime: String!
-        returnTime: String!
-        departureStation: Station!
-        returnStation: Station!
+        departure: DateTime!
+        return: DateTime!
+        departureStationId: Int!
+        returnStationId: Int!
         "The distance in meters"
         coveredDistance: Int!
         "The duration in seconds"
         duration: Int!
-        
-        
     }
+	
+	scalar DateTime
 `;
 
 export { typeDefs };
