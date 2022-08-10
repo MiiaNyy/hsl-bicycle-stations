8112ff7a9ce7185ddc48300e927f6428f6f911b7
MiiaNyy <mi.nyyssonen@gmail.com>
2022-08-10 14:15:39 +0300

    Added a navigation bar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,88 +1,42 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import { gql, useQuery } from "@apollo/client";
-import { Table } from "react-bootstrap";
 
-const GET_JOURNEYS = gql`
-    query Query($amount: Int!) {
-        getJourneys(amount: $amount) {
-            duration
-            coveredDistance
-            returnStation {
-                name
-				stationId
-            }
-            departureStation {
-                name
-				stationId
-            }
-        }
-    }`;
+import JourneysTable from "./components/JourneysTable";
+import StationsTable from "./components/StationsTable";
+import Navigation from "./components/Navigation";
 
-
-function JourneysTable () {
-	const { loading, error, data } = useQuery( GET_JOURNEYS, {
-		variables : { amount : 20 },
-	} );
-	
-	if ( loading ) return <p>Loading...</p>;
-	if ( error ) return <p>Error :(</p>;
-	
-	
-	return (
-		<Table striped bordered>
-			<thead>
-			<tr>
-				<th>Covered Distance (km)</th>
-				<th>Duration (min)</th>
-				<th>Departure station</th>
-				<th>Return station</th>
-				<th></th>
-			</tr>
-			</thead>
-			<tbody>
-			{ data.getJourneys.map( journey => (
-				<TableRow key={ journey.id } journey={ journey } />
-			) ) }
-			</tbody>
-		</Table>
-	)
-}
-
-function TableRow ( {journey} ) {
-	const departureStation = journey.departureStation;
-	const returnStation = journey.returnStation;
-	
-	return (
-		<tr key={ journey.id }>
-			<td>{ journey.coveredDistance }</td>
-			<td>{ journey.duration }</td>
-			<td><a href={"station/" + departureStation.stationId }>{ departureStation.name }</a> </td>
-			<td><a href="">{ returnStation.name }</a> </td>
-			<td><button>➡</button></td>
-		</tr>
-	);
-}
 
 function Home () {
+	
+	const border = {
+		border : "2px solid pink",
+		
+	};
+	
+	
 	return (
-		<Container>
-			<Row>
-				<h2>My first Apollo app 🚀</h2>
-			</Row>
-			<Row>
-				<Col md>
-					1 of 2
-				</Col>
-				<Col md>
-					2 of 2
-					<JourneysTable/>
-				</Col>
-			</Row>
-		</Container>
+		<>
+			<Navigation/>
+			<Container style={ border }>
+				<Row>
+					<h2>Helsinki city bicycle data</h2>
+				</Row>
+				<Row>
+					<Col md>
+						<h2>Stations</h2>
+						<StationsTable/>
+					</Col>
+					<Col md style={ border }>
+						<h2>Journeys</h2>
+						<JourneysTable/>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	
 	);
-};
+}
 
 
 export default Home;
---
+++ client/src/pages/components/Navigation.js
@@ -0,0 +1,33 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Button, Form } from "react-bootstrap";
+
+function Navigation() {
+	return (
+		<Navbar bg="light" expand="lg">
+			<Container>
+				<Navbar.Brand href="/">HSL bicycles</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#link">Link</Nav.Link>
+					</Nav>
+					<Form className="d-flex">
+						<Form.Control
+							type="search"
+							placeholder="Search"
+							className="me-2"
+							aria-label="Search"
+						/>
+						<Button variant="warning">Search</Button>
+					</Form>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default Navigation;
